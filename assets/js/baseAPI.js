2bb5fc6aeda041c7eece1d28f9dab56e09b1fe74
huang <1192628459@qq.com>
2022-08-04 11:24:21 +0800

    公共样式完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,6 +3,11 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 const baseUrl='http://www.liulongbin.top:3007'
 $.ajaxPrefilter(option=>{
+    if (option.url.includes('/my/')) {
+        option.headers={
+            Authorization:localStorage.getItem('token')
+        }
+    }
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url=baseUrl + option.url
 })
